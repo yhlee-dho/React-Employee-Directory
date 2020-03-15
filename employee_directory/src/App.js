8b572bf2fe41d4f84fc2c25f4a3269dbf26c9da4
unknown <yhlee89.dho@gmail.com>
2020-03-14 20:54:15 -0500

    redo without utils/api.js

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,34 +1,50 @@
-import React, {useState, useEffect} from "react";
-import Main from "./pages/Index";
-import Table from "./components/Table";
-import SearchBar from "./components/search";
+
+// import React, {useState, useEffect} from "react";
+// import Main from "./pages/Index";
+// import SearchBar from "./components/search";
+// import api from "./utils/API";
+
+import React from "react";
+import Header from "./components/header"
+import Table from "./components/table";
+import Footer from "./components/footer"
 
 import "./App.css";
 
-import api from "./utils/API";
-
 function App() {
-  const[users, setUsers]=useState();
-  const[search, setSearch]=useState();
-
-  const handleInputChange = e => {
-    setSearch(e.target.value);
-    console.log(search)
-  };
-
-  useEffect(() => {
-    api.allUsers(15).then(res => {
-      setUsers(res.data.results);
-    }).catch(err => console.log(err))
-  },[]);
-
   return (
     <div className="App">
-      <Main/>
-      <SearchBar users={users} search={search}/>
-      <Table users={users} search={search} handleInputChange={handleInputChange}/>
+      <Header />
+      <Table />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   const[users, setUsers]=useState([]);
+//   const[search, setSearch]=useState(``);
+
+//   const handleInputChange = e => {
+//     setSearch(e.target.value);
+//     console.log(search)
+//   };
+
+//   useEffect(() => {
+//     api.allUsers(15).then(res => {
+//       setUsers(res.data.results);
+//     }).catch(err => console.log(err))
+//   },[]);
+
+//   return (
+//     <div className="App">
+//       <Main/>
+//       <SearchBar users={users} search={search}/>
+//       <Table users={users} search={search} handleInputChange={handleInputChange}/>
+//     </div>
+//   );
+// }
+
+// export default App;
